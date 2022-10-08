6460c0b6937814886c89ccaab8e98e6e20053d0f
rushill2 <67648243+rushill2@users.noreply.github.com>
2022-10-08 14:40:18 -0500

    Create isBalancedTree.py

---
+++ Trees/Easy/isBalancedTree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if root is None:
+            return True
+        else:
+            return self.isBalanced(root.left) and self.isBalanced(root.right) and abs(self.maxDepth(root.left) - self.maxDepth(root.right))<=1
+
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
